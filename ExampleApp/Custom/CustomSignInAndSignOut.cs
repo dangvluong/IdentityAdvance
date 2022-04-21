d365dfa717b7a92d157083ecb50d349eb79b84dc
Luong Dang <dangvluong@gmail.com>
2022-04-21 13:54:40 +0700

    Working with ASP.NETCore

---
+++ ExampleApp/Custom/CustomSignInAndSignOut.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+namespace ExampleApp.Custom
+{
+    public class CustomSignInAndSignOut
+    {
+        public static async Task SignIn(HttpContext context)
+        {
+            string user = context.Request.Query["user"];
+            if (user != null)
+            {
+                Claim claim = new Claim(ClaimTypes.Name, user);
+                ClaimsIdentity ident = new ClaimsIdentity("qsv");
+                ident.AddClaim(claim);
+                await context.SignInAsync(new ClaimsPrincipal(ident));
+                await context.Response
+                .WriteAsync($"Authenticated user: {user}");
+            }
+            else
+            {
+                await context.ChallengeAsync();
+
+            }
+        }
+        public static async Task SignOut(HttpContext context)
+        {
+            await context.SignOutAsync();
+            await context.Response.WriteAsync("Signed out");
+        }
+    }
+}
--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -1,16 +1,36 @@
+using ExampleApp;
+using ExampleApp.Custom;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddAuthentication(opts => {
+    opts.DefaultScheme
+  = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie(opts => {
+    opts.LoginPath = "/signin";
+    opts.AccessDeniedPath = "/signin/403";
+});
+builder.Services.AddAuthorization();
 var app = builder.Build();
 app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
+//app.UseMiddleware<CustomAuthentication>();
+app.UseAuthentication();
+app.UseMiddleware<RoleMemberships>();
 app.UseRouting();
+app.UseMiddleware<ClaimsReporter>();
+//app.UseMiddleware<CustomAuthorization>();
+app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello world!"));
+    endpoints.MapGet("/secret", SecretEndpoint.Endpoint).WithDisplayName("secret");
     endpoints.MapRazorPages();
     endpoints.MapDefaultControllerRoute();
+    //endpoints.Map("/signin", CustomSignInAndSignOut.SignIn);
+    //endpoints.Map("/signout", CustomSignInAndSignOut.SignOut);
 });
 
 
