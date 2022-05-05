a483f63ee3c8bd409df49a49b3e7eda52ab45958
Luong Dang <dangvluong@gmail.com>
2022-05-05 17:49:03 +0700

    Chapter 16: Creating a User Store

--- ExampleApp/Controllers/HomeController.cs
+++ ExampleApp/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace ExampleApp.Controllers
     public class HomeController : Controller
     {
         public IActionResult Test() => View();
-        [Authorize(Roles = "User", AuthenticationSchemes = "OtherScheme")]
+        //[Authorize(Roles = "User", AuthenticationSchemes = "OtherScheme")]
         public IActionResult Protected() => View("Test", "Protected Action");
         [AllowAnonymous]
         public IActionResult Public() => View("Test", "Unauthenticated Action");
--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -1,20 +1,19 @@
 using ExampleApp;
 using ExampleApp.Custom;
+using ExampleApp.Identity;
+using ExampleApp.Identity.Store;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<IAuthorizationHandler, CustomRequirementHandler>();
-builder.Services.AddRazorPages(opt =>
-{
-    opt.Conventions.AuthorizePage("/Secret", "NotAdmins");
-});
-builder.Services.AddControllersWithViews(opts => {
-    opts.Conventions.Add(new AuthorizationPolicyConvention("Home",
-    policy: "NotAdmins"));
-    opts.Conventions.Add(new AuthorizationPolicyConvention("Home",
-    action: "Protected", policy: "UsersExceptBob"));
-});
+//builder.Services.AddTransient<IAuthorizationHandler, CustomRequirementHandler>();
+builder.Services.AddSingleton<ILookupNormalizer, Normalizer>();
+builder.Services.AddSingleton<IUserStore<AppUser>, UserStore>();
+builder.Services.AddIdentityCore<AppUser>();
+builder.Services.AddSingleton<IUserValidator<AppUser>, EmailValidator>();
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(opts => {
     opts.DefaultScheme
   = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -36,13 +35,16 @@ app.UseRouting();
 //app.UseMiddleware<ClaimsReporter>();
 //app.UseMiddleware<CustomAuthorization>();
 //app.UseAuthorization();
-app.UseMiddleware<AuthorizationReporter>();
+//app.UseMiddleware<AuthorizationReporter>();
+app.UseMiddleware<RoleMemberships>();
+app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello world!"));
+    //endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello world!"));
     //endpoints.MapGet("/secret", SecretEndpoint.Endpoint).WithDisplayName("secret");
     endpoints.MapRazorPages();
     endpoints.MapDefaultControllerRoute();
+    endpoints.MapFallbackToPage("/Secret");
     //endpoints.Map("/signin", CustomSignInAndSignOut.SignIn);
     //endpoints.Map("/signout", CustomSignInAndSignOut.SignOut);
 });
